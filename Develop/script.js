1070135699f4632c4689e539981031b6275464dc
MilkyRamen <127574535+MilkyRamen@users.noreply.github.com>
2023-04-06 02:44:24 -0400

    added code to make the generator functional and updated README

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,54 @@
 // Assignment code here
 
 
+
+function generatePassword(){
+  let passwordLength = prompt("How long is the password? (between 8 and 128)");
+  if (passwordLength < 8 || passwordLength > 128 ){
+      return alert("choose a number between 8 and 128");
+  } 
+
+  if (passwordLength >= 8 && passwordLength <= 128){
+    var uppercaseChars = confirm("uppercase characters?");
+    var lowercaseChars = confirm("lowercase characters?");
+    var numberChars = confirm("number characters?");
+    var specialChars = confirm("special characters?");
+  }
+
+  if (uppercaseChars != true && lowercaseChars != true && numberChars != true && specialChars != true){
+    return alert("must select at least one character type");
+  }
+
+var characters = "";
+if (uppercaseChars){
+  characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+if (lowercaseChars){
+  characters += "abcdefghijklmnopqrstuvwxyz";
+}
+
+if (numberChars){
+  characters += "0123456789";
+}
+
+if (specialChars){
+  characters += "!@#$%^&*()_+";
+}
+
+let password = ""
+for (let i =0; i < passwordLength; i++) {
+  var randomIndex = Math.floor(Math.random() * characters.length);
+  password += characters.charAt(randomIndex);
+}
+
+return "your password is " + password;
+
+
+
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Password Generator Starter Code
-i hope this works
+
+## Description
+simple website used to create a random password based on selected criteria
+
+## Usage
+can be used to create a random password with inputs for selecting desired length and character types
+
+## Credits
+used code from https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working for help and inspiration
+
+## Liscense
+MIT Liscense
+
+## Deployed repo and website link
